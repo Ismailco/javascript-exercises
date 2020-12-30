ccd110723780c8e0b99efdddfdc5c60fcdb29ca0
Ismail courr <ismailcourr@gmail.com>
2020-12-30 19:08:08 +0100

    fix helloWorld, repeatString, and reverseString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, time) {
+    let result = "";
+    if (time < 0) {
+        return "ERROR";
+    } else {
+        for (i = 0; i < time; i++) {
+            result = result + string;
+        }
+        return result;
+    }
+};
 
-}
-
-module.exports = repeatString
+module.exports = repeatString;
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let splitedString = string.split("");
+    let newString = [];
+    for (i = string.length - 1; i >= 0; i--) {
+        newString.push(splitedString[i]);
+    }
+    return newString.join("");
+};
 
-}
-
-module.exports = reverseString
+module.exports = reverseString;
